4742eceb025921508dc19ecf6dae0a490edd3352
zoueature <zoueature@gmail.com>
2022-06-09 18:24:28 +0800

    complete dark mode in other website

--- src/content/main.js
+++ src/content/main.js
@@ -1,6 +1,7 @@
 /* eslint-disable */
 import { createApp } from 'vue'
 import app from './components/app.vue'
+import keys from "@/popup/store/keys";
 
 console.log(11111111111111, 'content inject')
 
@@ -18,9 +19,27 @@ function joinContent (element) {
     document.body.appendChild(div)
     createApp(element).mount('#joinContentApp')
 }
+// import dark from "@/content/components/dark";
 
+// updateBackground 黑暗模式
 function updateBackground() {
-    const div = document.createElement('div')
-    div.className = 'dark-cover'
-    // document.body.appendChild(div)
+    chrome.storage.local.get([keys.config], function(result) {
+        console.log(result[keys.config].darkModel)
+        if (result[keys.config].darkModel) {
+            let html = document.getElementsByTagName('html')[0]
+            html.style.filter = "invert(1) hue-rotate(180deg)"
+            html.style.WebkitFilter = "invert(1) hue-rotate(180deg)";
+
+            console.log(html.style.filter)
+            let img = document.getElementsByTagName('img')
+            for (let i = 0; i < img.length; i ++) {
+                img[i].style.filter = "invert(1) hue-rotate(180deg)"
+            }
+            let video = document.getElementsByTagName('video')
+            for (let i = 0; i < video.length; i ++) {
+                video[i].style.filter = "invert(1) hue-rotate(180deg)"
+            }
+        }
+    })
 }
+
