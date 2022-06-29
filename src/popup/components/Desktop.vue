21d09a45f09881eef069191a0c11d1cdbebf9c54
zoueature <zoueature@gmail.com>
2022-06-29 15:38:06 +0800

    remove useless img

--- src/popup/components/Desktop.vue
+++ src/popup/components/Desktop.vue
@@ -26,7 +26,7 @@
     <App></App>
     <RightDrawer></RightDrawer>
     <div class="setting" @click="openSetting">
-      <img src="../../assets/icon/setting.png" alt="setting" style="width: 100%; height: 100%">
+      <img src="../../assets/icon/logo.png" alt="setting" style="width: 100%; height: 100%">
     </div>
   </div>
 </template>
