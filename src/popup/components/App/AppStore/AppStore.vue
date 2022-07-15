c40e447a0df57a09833e5ed50ccd6f7292e9fa66
zoueature <zoueature@gmail.com>
2022-07-15 23:06:13 +0800

    alone diy app

--- src/popup/components/App/AppStore/AppStore.vue
+++ src/popup/components/App/AppStore/AppStore.vue
@@ -49,78 +49,23 @@
         </div>
       </ul>
     </div>
-    <div v-else class="app-list diy-app">
-      <div class="title diy-title">
-        自定义应用
-      </div>
-      <div class="app-input">
-        <div>地址</div>
-        <input placeholder="http://" v-model="diyApp.link" @focusout="getWebsiteTitle" @keyup.enter="getWebsiteTitle" class="input-item">
-      </div>
-      <div class="app-input">
-        <div>名称</div>
-        <input placeholder="输入名称" v-model="diyApp.name" class="input-item">
-      </div>
-      <div class="select-app-icon">
-        <div class="app-icon-shower" :style="'background: '  + diyApp.wordIconColor" @click="diyApp.iconType = 'word'">
-          {{diyApp.name.substring(0,2)}}
-          <div class="selected" v-if="diyApp.iconType === 'word'">
-            <img src="../../../../assets/icon/gou_white_fill.png" alt="" style="width: 100%; height: 100%; margin: 0; padding: 0">
-          </div>
-        </div>
-        <div class="app-icon-shower" v-if="diyApp.onlineIcon !== ''" @click="diyApp.iconType = 'img'">
-         <img :src="diyApp.onlineIcon" style="width: 100%; height: 100%" >
-          <div class="selected" v-if="diyApp.iconType === 'img'">
-            <img src="../../../../assets/icon/gou_white_fill.png" alt="" style="width: 100%; height: 100%; margin: 0; padding: 0">
-          </div>
-        </div>
-      </div>
-      <div class="select-icon-color">
-        <div class="color-shower" v-for="(color, index) in colors"
-             :key="index"
-             :style="'background: ' + color.color"
-             @click="diyApp.wordIconColor = color.color"
-        >
-          <check theme="outline" size="20" fill="#fff" :strokeWidth="2" v-if="diyApp.wordIconColor === color.color"/>
-        </div>
-      </div>
-      <div class="opt-container">
-        <div class="submit-app" @click="addDiyApp">添加</div>
-      </div>
-    </div>
+    <diy-app v-else class="app-list"></diy-app>
   </div>
 </template>
 
 <script>
-function formatLink(link) {
-  let requestLink = link
-  if (requestLink.substring(0, 7) !== "http://" && requestLink.substring(0, 8) !== 'https://') {
-    requestLink = "http://" + requestLink
-  }
-  return requestLink
-}
 
-import color from "@/popup/components/App/AppStore/color";
-import utils from "@/utils/funcs"
 import api from "@/popup/components/api/app"
-import {Check} from "@icon-park/vue-next"
 import LoadingInline from "@/popup/components/common/LoadingInline"
+import DiyApp from "@/popup/components/App/AppStore/DiyApp"
 
 const diyCategoryId = 7
-
-const defaultDiyApp = {
-  link: '',
-  name: '',
-  wordIcon: '',
-  wordIconColor: color[0].color,
-  onlineIcon: '',
-}
 
 export default {
   name: "AppStore",
   components: {
-    Check,
     LoadingInline,
+    DiyApp,
   },
   methods: {
     search(e) {
@@ -193,48 +138,6 @@ export default {
       }
       window.open(app.link, '_blank');
     },
-    getWebsiteTitle() {
-      let that = this
-      if (this.diyApp.link !== "") {
-        let requestLink = this.diyApp.link
-        requestLink = formatLink(requestLink)
-        let url = new URL(requestLink)
-        this.diyApp.onlineIcon = url.origin + "/favicon.ico"
-        this.$http.get(requestLink).then((res) => {
-          if (res.status === 200) {
-            let title = utils.getTitleFromHTML(res.data)
-            if (title !== "") {
-              that.diyApp.name = title
-            }
-          }
-        })
-      }
-    },
-    addDiyApp() {
-      let app = {
-        id: new Date().getTime(),
-        name: this.diyApp.name,
-        icon: this.diyApp.onlineIcon,
-        app: '',
-        link: formatLink(this.diyApp.link),
-        background: this.diyApp.wordIconColor,
-        iconType: this.diyApp.iconType
-      }
-      if (app.iconType === '') {
-        app.iconType = 'word'
-      }
-      if (app.iconType === 'word') {
-        app.icon = ''
-      }
-      this.$store.commit('addApp', app)
-      this.diyApp = {
-        link: defaultDiyApp.link,
-        name: defaultDiyApp.name,
-        wordIcon: defaultDiyApp.wordIcon,
-        wordIconColor: defaultDiyApp.wordIconColor,
-        onlineIcon: defaultDiyApp.onlineIcon,
-      }
-    }
   },
   created() {
     this.getAppCategoryList()
@@ -247,15 +150,6 @@ export default {
       apps: [],
       installedApp: this.$store.getters.installedAppID,
       diyCategoryId: diyCategoryId,
-      diyApp: {
-        link: defaultDiyApp.link,
-        name: defaultDiyApp.name,
-        wordIcon: defaultDiyApp.wordIcon,
-        wordIconColor: defaultDiyApp.wordIconColor,
-        onlineIcon: defaultDiyApp.onlineIcon,
-        iconType: '',
-      },
-      colors: color,
       keyword: '',
       page: 1, 
       size: 15,
@@ -445,76 +339,6 @@ export default {
     -webkit-line-clamp: 2;
     -webkit-box-orient: vertical;
   }
-  .app-input {
-    width: 90%;
-    text-align: left;
-    margin: 0 auto;
-    font-size: 14px;
-  }
-  .app-input input {
-    margin-top: 10px;
-    border: none;
-    outline: none;
-    height: 25px;
-    width: 70%;
-    background: none;
-    border-bottom: 1px solid rgba(0, 0, 0, 0.25);
-    font-size: 15px;
-  }
-  .input-item::placeholder {
-    color: rgba(0, 0, 0, 0.2)
-  }
-  .select-app-icon {
-    width: 90%;
-    margin: 0 auto;
-  }
-  .app-icon-shower {
-    width: 70px;
-    height: 70px;
-    float: left;
-    margin-right: 10px;
-    color: white;
-    align-items: center;
-    text-align: center;
-    line-height: 70px;
-    font-size: 25px;
-    font-weight: bolder;
-    overflow: hidden;
-    border-radius: 5px;
-    position: relative;
-  }
-  .select-icon-color {
-    display: flex;
-    width: 90%;
-    margin: 0 auto;
-    height: 34px;
-    clear: both;
-    justify-content: space-between;
-  }
-  .color-shower {
-    width: 25px;
-    height: 25px;
-    border-radius: 4px;
-    display: flex;
-    justify-items: center;
-    justify-content: center;
-    align-items: center;
-    align-content: center;
-  }
-  .opt-container {
-    width: 90%;
-    margin: 0 auto;
-  }
-  .submit-app {
-    width: 25%;
-    height: 40px;
-    /*margin-left: 5%;*/
-    background: #42b983;
-    border-radius: 7px;
-    font-size: 15px;
-    line-height: 40px;
-    color: white;
-  }
   .title {
     width: 90%;
     margin: 0 auto;
@@ -522,9 +346,6 @@ export default {
     text-align: left;
     font-size: 25px;
     /* margin-bottom: 25px; */
-  }
-  .diy-title {
-    font-size: 20px;
   }
   .preview {
     top: 50%;
